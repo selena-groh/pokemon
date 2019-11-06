216fd9150c41b059f5cbdaacf41415e22977baf7
Selena Groh <selena.groh@gmail.com>
2019-11-06 01:45:12 -0500

    Updates

---
+++ precache-manifest.a8cf933d5d4c5287ba7e146ad9383176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31a844a7c4bc23ec6e598659212d91d",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "28e853998ba6dac6962f",
+    "url": "/pokemon/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "71e34c5a1fdc7cad0e34",
+    "url": "/pokemon/static/js/2.61ade839.chunk.js"
+  },
+  {
+    "revision": "28e853998ba6dac6962f",
+    "url": "/pokemon/static/js/main.e8fa4b6a.chunk.js"
+  },
+  {
+    "revision": "47f0078611fdfae4e957",
+    "url": "/pokemon/static/js/runtime-main.29f8b35f.js"
+  }
+]);
